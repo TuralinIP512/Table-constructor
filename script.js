61150ff2b6af16ebc610186595228d6d84ffd85d
Туралин А.С. ИП-512 <ttt8641f@yandex.ru>
2026-05-16 13:29:43 +0700

    feat: add alert test to script to verify button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Testing...");
+window.onload = function() {
+    let button = document.getElementById("addBtn");
+    let subjectInput = document.getElementById("subject");
+    let daySelect = document.getElementById("day");
+    let slotInput = document.getElementById("slot");
+
+    button.onclick = function() {
+        let subject = subjectInput.value;
+        let day = daySelect.value;
+        let slot = slotInput.value;
+
+        alert("Предмет: " + subject + "\nДень: " + day + "\nПара: " + slot);
+    };
+};
